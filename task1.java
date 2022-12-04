9e7d15ca91b4a3bbe41fac9b81d366e93134ea52
Elena <malkovva@gmail.com>
2022-12-04 18:04:12 +0300

    Added solution to task 1

---
+++ task1.java
@@ -0,0 +1,32 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+// Дана строка sql-запроса "select * from students where ".
+// Сформируйте часть WHERE этого запроса, используя StringBuilder.
+// Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+public class task1 {
+
+    public static void main(String[] args) {
+        String[] db = { "name:Ivanov", "country:Russia", "city:Moscow", "age:null" };
+        String inquiry = "select * from students where ";
+        StringBuilder sb = new StringBuilder(inquiry);
+        for (int i = 0; i <= db.length - 1; i++) {
+            db[i].replace('"', ' '); // избавились от кавычек
+            String[] elements = db[i].split(":"); // разбили каждый элемент db на новый массив из 2 строк
+            if (elements[1] != "null" & i < db.length - 2) {
+                sb.append(elements[0]).append(" = ").append(elements[1]).append(" and "); // добавили and если элемент
+                                                                                          // не последний
+            }
+            if (elements[1] != "null" & i == db.length - 2) {
+                sb.append(elements[0]).append(" = ").append(elements[1]);
+            }
+            ;
+
+        }
+        System.out.println(sb);
+    }
+
+}
